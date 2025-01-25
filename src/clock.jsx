5800294e050e1bdc8b69225743c8388bcac46e22
Chhavi Paliwal <009ankitkalirawana@gmail.com>
2025-01-25 14:27:42 +0530

    initialize React app with Vite, Tailwind CSS, and basic routing

---
+++ src/clock.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer); // Cleanup timer
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen font-sans text-3xl bg-[#282c34] text-white">
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
+  );
+}
+
+export default Clock;
